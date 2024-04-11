a47df95e7b929338a477b78f605745abbd762499
patthoege <paty.hoge@gmail.com>
2024-04-11 21:16:02 +0200

    added optimizations to this state

--- src/App.js
+++ src/App.js
@@ -26,9 +26,19 @@ class App extends Component {
     );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
+  }
+
     render () {
-      const filteredMonsters = this.state.monsters.filter((monster) => {
-        return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+      const { monsters, searchField } = this.state;
+      const { onSearchChange } = this;
+
+      const filteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLocaleLowerCase().includes(searchField);
       });
 
       return (
@@ -37,12 +47,7 @@ class App extends Component {
           className='search-box' 
           type='search' 
           placeholder='search monsters' 
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            })
-          }} 
+          onChange={onSearchChange}
           />
 
           {
