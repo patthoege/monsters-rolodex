77705802434a9a3fc95771a27ef966e97b80b124
patthoege <paty.hoge@gmail.com>
2024-04-11 17:06:47 +0200

    componentDidMount method to fetch api into json format

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,25 +6,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Patty',
-          id: '123'
-        },
-        {
-          name: 'Mila',
-          id: '1234'
-        },
-        {
-          name: 'Yash',
-          id: '12345'
-        },
-        {
-          name: 'Bhumi',
-          id: '12465463'
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => 
+      this.setState(
+        () => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
   }
 
     render () {
@@ -34,7 +30,11 @@ class App extends Component {
         <div className="App">
           {
             this.state.monsters.map((monster) => {
-              return <div key={monster.id}><h1>{monster.name}</h1></div>;
+              return (
+                <div key={monster.id}>
+                  <h1>{monster.name}</h1>
+              </div>
+              );
             })
           }
         </div>
